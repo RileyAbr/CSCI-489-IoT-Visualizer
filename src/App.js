8834fbc3e84766b95a6da779b63434167d712cd4
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-11-30 22:31:46 -0600

    Create IoT demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import packetBarData from "./packet_bar_data.json";
+import packetLineData from "./packet_line_data.json";
+import {
+    BarChart,
+    Bar,
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip
+} from "recharts";
+
+const labelStyling = {
+    color: "black"
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <h1>Packet Sniffers 👃</h1>
+            <section>
+                <BarChart
+                    width={1800}
+                    height={800}
+                    data={packetBarData}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 10 }}
+                >
+                    <XAxis dataKey="mac_address" hide={true} />
+                    <YAxis />
+                    <Tooltip labelStyle={labelStyling} />
+                    <Bar dataKey="count" fill="#8884d8" />
+                </BarChart>
+            </section>
+            <section>
+                <LineChart
+                    width={1800}
+                    height={800}
+                    data={packetLineData}
+                    margin={{ top: 10, right: 30, left: 20, bottom: 5 }}
+                >
+                    <XAxis
+                        dataKey="hour"
+                        tickFormatter={(label) => `${label}:00`}
+                    />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip labelStyle={labelStyling} />
+                    <Line
+                        type="monotone"
+                        dataKey="count"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                </LineChart>
+            </section>
+        </main>
+    );
 }
 
 export default App;
